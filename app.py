30df5855362628401c903902a489b814b42a92d2
KTachibanaM <whj19931115@gmail.com>
2024-07-31 07:19:54 +0000

    capture bad request

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 import os
 import sentry_sdk
+import werkzeug.exceptions
+from sentry_sdk import capture_exception
 from flask import Flask, g
+from dotenv import load_dotenv
 from yurikamome.mastodon_meta_blueprint import meta_blueprint
 from yurikamome.mastodon_timelines_blueprint import timelines_blueprint
 from yurikamome.pages_blueprint import pages_blueprint
 from yurikamome.helpers import get_db
 
+load_dotenv()
 
 if os.getenv('SENTRY_DSN'):
     sentry_sdk.init(
-        dsn=os.getenv('SENTRY_DSN'),
+        dsn=os.getenv('SENTRY_DSN')
     )
-
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.register_blueprint(pages_blueprint, url_prefix='/')
 app.register_blueprint(meta_blueprint, url_prefix='/')
 app.register_blueprint(timelines_blueprint, url_prefix='/')
+
+
+@app.errorhandler(werkzeug.exceptions.BadRequest)
+def handle_bad_request(e):
+    capture_exception(e)
+    return 'bad request!', 400
 
 
 @app.teardown_appcontext
